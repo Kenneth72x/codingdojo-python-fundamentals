ceb1c44347403fb8de3593719e2ec20d3976eec6
Kenneth Rothstein <skottrod@me.com>
2021-04-04 07:11:42 -0700

    Multiples of Five

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -4,3 +4,9 @@ def basic():
         print(i)
 
 basic() #don't forget, the function needs to be called. 
+
+#Multiples of Five: Print all the multiples of 5 from 5 to 1,000
+def multiples_of_five():
+    for i in range(5, 1001, 5):
+        print(i)
+multiples_of_five()
