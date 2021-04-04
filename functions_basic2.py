a9e11ab5a713af451bdfd9039fe4cf3e22d2fb28
Kenneth Rothstein <skottrod@me.com>
2021-04-04 10:56:33 -0700

    Print and Return

--- functions_basic2.py
+++ functions_basic2.py
@@ -10,3 +10,10 @@ def countdown(num):
 print(countdown(30))
 print(countdown(5)) 
 print(countdown(15))
+
+#Print and Return: Create a function that will receive a list with two numbers. Print the first value and return the second. 
+#Example: print_and_return([1,2]) should pring 1 and return 2. 
+def print_and_return(nums_list):
+    print(nums_list[0])
+    return nums_list[1]
+print(print_and_return([1,2]))
