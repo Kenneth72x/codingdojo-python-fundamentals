bcd8b015f2d54a0a6e0b314ab68b024b0c601053
Kenneth Rothstein <skottrod@me.com>
2021-04-04 07:21:47 -0700

    Counting, Coding, Coding Dojo

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -9,4 +9,17 @@ basic() #don't forget, the function needs to be called.
 def multiples_of_five():
     for i in range(5, 1001, 5):
         print(i)
+
 multiples_of_five()
+
+#Counting, the Dojo Way: Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+def the_dojo_way():
+    for i in range(1,101):
+        if i % 10 == 0:
+            print("Coding Dojo")
+        elif i % 5 == 0:
+            print("Coding")
+        else: 
+            print(i)
+
+the_dojo_way()
